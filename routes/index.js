349376c4ad52ba4e551cdc1d0e80df7e6cd631de
Evan Prodromou <evan@status.net>
2012-07-01 15:08:59 -0700

    Get a bunch of stuff

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,30 @@
-var person_db = require('../lib/Person');
+var person_db = require('../lib/Person'),
+    Person = person_db.Person,
+    async = require('async');
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-    res.render('index', { title: 'Express' })
+    res.render('index', { title: 'Express' });
 };
 
 exports.search = function(req, res) {
-  answer = {}
+    var ids = req.body.ids,
+        getOthers = function(person, callback) {
+            person.getIdentifiers(callback);
+        };
 
-  req.body.ids.forEach(function(id) {
-    console.log("searching on "+id);
-    person_db.Person.fromIdentifier(id, function(err,data){callback(id,err,data)});
-  })
+    async.map(ids, Person.fromIdentifier, function(err, people) {
+        async.map(people, getOthers, function(err, identlist) {
+            var i, map = {};
+            for (i = 0; i < ids.length; i++) {
+                map[ids[i]] = identlist[i];
+            }
+            res.json(map);
+        });
+    });
+};
 
-  function callback(id, err, data) {
-    answer[id] = [];
-    if(err) {
-      answer.status = "ERR"
-    } else {
-      answer.status = "OK"
-    }
-    // if req.body.ids.length == the_end, then answer
-    //res.json(answer)
-  }
-
-}
 
