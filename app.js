1503364685147dd6f7741b53245079b651c3f7f4
Ben Atkin <ben@benatkin.com>
2012-07-01 14:35:57 -0700

    whitespace

--- app.js
+++ app.js
@@ -14,19 +14,19 @@ var express = require('express')
 var app = module.exports = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
--- test/app_test.js
+++ test/app_test.js
@@ -2,27 +2,27 @@ var spawn = require('child_process').spawn
   , request = require('request');
 
 describe('app', function() {
-  var child;
+    var child;
 
-  before(function(done) {
-    child = spawn('node', ['app.js']);
-    child.stdout.on('data', function(data) {
-      setTimeout(done, 50);
+    before(function(done) {
+        child = spawn('node', ['app.js']);
+        child.stdout.on('data', function(data) {
+            setTimeout(done, 50);
+        });
+        child.stderr.pipe(process.stderr);
     });
-    child.stderr.pipe(process.stderr);
-  });
 
-  after(function(done) {
-    child.on('exit', function() {
-      done();
+    after(function(done) {
+        child.on('exit', function() {
+            done();
+        });
+        child.kill();
     });
-    child.kill();
-  });
 
-  it('should have a home page', function(done) {
-    request('http://localhost:3000/', function(err, resp, body) {
-      resp.statusCode.should.equal(200);
-      done();
+    it('should have a home page', function(done) {
+        request('http://localhost:3000/', function(err, resp, body) {
+            resp.statusCode.should.equal(200);
+            done();
+        });
     });
-  });
 });
