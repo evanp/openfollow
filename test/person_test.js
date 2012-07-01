1dc3cb759f9ee3873c7f162f1409389c4ddced4a
Evan Prodromou <evan@status.net>
2012-07-01 14:25:27 -0700

    Add a create and get test

--- test/person_test.js
+++ test/person_test.js
@@ -1,6 +1,9 @@
+var assert = require("assert");
+
 describe('person', function() {
 
-  var Person = require('../lib/Person').Person;
+  var Person = require('../lib/Person').Person,
+      Identifier = require('../lib/Identifier').Identifier;
 
   before(function(done) {
       var databank = require('databank'),
@@ -20,5 +23,12 @@ describe('person', function() {
 
   it('should fail to find an unknown person', function(done) {
       Person.get("unknown", done);
+  });
+
+  it('should automatically add a uuid to a new person', function(done) {
+      Person.create({}, function(err, person) {
+          assert.ok(person);
+          assert.ok(person.uuid);
+      });
   });
 });
