b2ab67f1112a6fe8940d112af97ba4d26122b3dc
Ben Atkin <ben@benatkin.com>
2012-07-01 14:06:51 -0700

    make tests run app; test home page

---
+++ test/app_test.js
@@ -0,0 +1,28 @@
+var spawn = require('child_process').spawn
+  , request = require('request');
+
+describe('app', function() {
+  var child;
+
+  before(function(done) {
+    child = spawn('node', ['app.js']);
+    child.stdout.on('data', function(data) {
+      setTimeout(done, 50);
+    });
+    child.stderr.pipe(process.stderr);
+  });
+
+  after(function(done) {
+    child.on('exit', function() {
+      done();
+    });
+    child.kill();
+  });
+
+  it('should have a home page', function(done) {
+    request('http://localhost:3000/', function(err, resp, body) {
+      resp.statusCode.should.equal(200);
+      done();
+    });
+  });
+});
