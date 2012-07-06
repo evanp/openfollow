83b12a8c07e5e3626c697a589436f81bc8f09e14
Evan Prodromou <evan@status.net>
2012-07-06 00:09:32 -0400

    Identifier.unify() method
    Added a method to "unify" two identifiers -- that is, make them belong
    to the same person. It's similar to unification in logic programming.
    
    This should make it easier when adding new edges to the graph.

--- test/identifier_test.js
+++ test/identifier_test.js
@@ -1,21 +1,55 @@
-var assert = require("assert");
+var assert = require("assert"),
+    async = require("async");
 
 describe('identifier', function() {
 
-    var Identifier = require('../lib/Identifier');
+    var Identifier = require('../lib/Identifier'),
+        Person = require('../lib/Person'),
+        people = new Array(10);
 
     before(function(done) {
         var databank = require('databank'),
             DatabankObject = databank.DatabankObject,
             Databank = databank.Databank,
-            db = Databank.get("memory", {});
+            db = Databank.get("memory", {}),
+            personMaker = function(i) {
+                return function(cb) {
+                    async.waterfall([
+                        function(cb) {
+                            Person.create({}, cb);
+                        },
+                        function(person, cb) {
+                            people[i] = person;
+                            Identifier.create({id: "http://example.com/bound" + i, person: person.uuid}, cb);
+                        },
+                        function(id, cb) {
+                            Identifier.create({id: "http://example.com/equivalent" + i, person: people[i].uuid}, cb);
+                        },
+                        function(id, cb) {
+                            cb(null);
+                        }
+                    ], cb);
+                };
+            };
 
         db.connect({}, function(err) {
             if (err) {
                 done(err);
             } else {
                 DatabankObject.bank = db;
-                done();
+                // Blech
+                async.parallel([
+                    personMaker(0),
+                    personMaker(1),
+                    personMaker(2),
+                    personMaker(3),
+                    personMaker(4),
+                    personMaker(5),
+                    personMaker(6),
+                    personMaker(7),
+                    personMaker(8),
+                    personMaker(9)
+                ], done);
             }
         });
     });
@@ -90,5 +124,64 @@ describe('identifier', function() {
         var can = Identifier.canonical("https://www.twitter.com/evanpro");
         assert.equal(can, "https://twitter.com/evanpro");
         done();
+    });
+
+    it('should have a unify() method', function(done) {
+        assert.ok(Identifier.unify);
+        done();
+    });
+
+    it('should unify two new identifiers with a new person', function(done) {
+        Identifier.unify("http://example.com/unbound0", "http://example.com/unbound1", function(err, uuid) {
+            assert.ifError(err);
+            assert.ok(uuid);
+            done();
+        });
+    });
+
+    it('should unify unbound second item with bound first item', function(done) {
+        Identifier.unify("http://example.com/bound0", "http://example.com/unbound2", function(err, uuid) {
+            assert.ifError(err);
+            assert.ok(uuid);
+            assert.equal(uuid, people[0].uuid);
+            done();
+        });
+    });
+
+    it('should unify unbound first item with bound second item', function(done) {
+        Identifier.unify("http://example.com/unbound3", "http://example.com/bound1", function(err, uuid) {
+            assert.ifError(err);
+            assert.ok(uuid);
+            assert.equal(uuid, people[1].uuid);
+            done();
+        });
+    });
+
+    it('should unify bound first item with equivalent bound second item', function(done) {
+        Identifier.unify("http://example.com/bound2", "http://example.com/equivalent2", function(err, uuid) {
+            assert.ifError(err);
+            assert.ok(uuid);
+            assert.equal(uuid, people[2].uuid);
+            done();
+        });
+    });
+
+    it('should error unifying two non-equivalent bound items', function(done) {
+        Identifier.unify("http://example.com/bound3", "http://example.com/bound4", function(err, uuid) {
+            assert.ok(err);
+            done();
+        });
+    });
+
+    it('should unify a second unbound item after first unbound item is bound', function(done) {
+        Identifier.unify("http://example.com/bound5", "http://example.com/unbound4", function(err, uuid) {
+            assert.ifError(err);
+            Identifier.unify("http://example.com/unbound4", "http://example.com/unbound5", function(err, uuid) {
+                assert.ifError(err);
+                assert.ok(uuid);
+                assert.equal(uuid, people[5].uuid);
+                done();
+            });
+        });
     });
 });
