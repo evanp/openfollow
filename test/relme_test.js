191f3d7a71b091971a6ef61987e83d9547668608
Evan Prodromou <evan@status.net>
2012-07-11 14:43:52 -0400

    More twiddling with test server

--- test/relme_test.js
+++ test/relme_test.js
@@ -10,16 +10,20 @@ describe("relme", function() {
 
     before(function(done) {
         app = express.createServer();
+
         app.get("/missing", function(req, res, next) {
             res.send("Not here", 404);
         });
+
         app.get("/image.png", function(req, res, next) {
             res.sendfile(__dirname + "/image.png");
         });
+
         app.get("/user1.html", function(req, res, next) {
             res.send("<html><head><title>User 1</title></head>"+
                      "<body><p>No rel-me links here.</p></body></html>", 200);
         });
+
         app.get("/user2.html", function(req, res, next) {
             res.send("<html><head><title>User 2</title></head>"+
                      "<body><ul>"+
@@ -31,13 +35,21 @@ describe("relme", function() {
                      "<li><a href='http://video.example.com/user2' rel='me video'>Video User 2</a></li>"+
                      "<li></ul></body></html>", 200);
         });
+
+        app.set('port', 4816);
+        app.use(app.router);
+
         app.on("listening", function() {
+            console.log("Listening");
             done();
         });
+
         app.on("error", function(err) {
+            console.log("Error");
             done(err);
         });
-        app.listen(4816);
+
+        app.listen(4816, "localhost");
     });
 
     it("should be a singleton object", function(done) {
