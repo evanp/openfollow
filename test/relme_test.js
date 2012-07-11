c7e4f291cd56b3e3ce6ede3f8a3ff78143fcd9e9
Evan Prodromou <evan@status.net>
2012-07-11 14:03:27 -0400

    Check the more extensive page

--- test/relme_test.js
+++ test/relme_test.js
@@ -73,4 +73,17 @@ describe("relme", function() {
             done();
         });
     });
+
+    it("should return the right links for a page with relme links", function(done) {
+        RelMe.getLinks("http://localhost:4816/user2.html", function(err, links) {
+            should.not.exist(err);
+            should.exist(links);
+            links.should.be.a("array");
+            links.should.have.length(3);
+            links.should.include("http://geo.example.com/user2");
+            links.should.include("http://user2.example.net/blog");
+            links.should.include("http://video.example.com/user2");
+            done();
+        });
+    });
 });
